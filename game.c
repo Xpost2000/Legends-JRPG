e0d0c045d748d1a38c76f7b4d16b81844b1f14f5
Jerry Zhu <xpost2000cod@gmail.com>
2022-06-30 16:53:15 -0400

    need to add editor code

--- game.c
+++ game.c
@@ -81,6 +81,13 @@ static string ui_pause_menu_strings[] = {
     string_literal("OPTIONS"),
     string_literal("QUIT"),
 };
+static string ui_pause_editor_menu_strings[] = {
+    string_literal("RESUME"),
+    string_literal("TEST"),
+    string_literal("SAVE"),
+    string_literal("OPTIONS"),
+    string_literal("QUIT"),
+};
 
 #include "entity.c"
 entity_id player_id;
@@ -104,16 +111,22 @@ struct game_state {
     /* NOTE main menu, or otherwise menus that occur in different states are not ui states. */
     u32 last_ui_state;
     u32 ui_state;
+
+    s32 in_editor;
+
     enum ui_pause_menu_animation_state{
         UI_PAUSE_MENU_TRANSITION_IN,
         UI_PAUSE_MENU_NO_ANIM,
         UI_PAUSE_MENU_TRANSITION_CLOSING,
     };
+
+    /* share this for all types of similar menu types. */
     struct ui_pause_menu {
         u8  animation_state; /* 0 = OPENING, 1 = NONE, 2 = CLOSING */
         f32 transition_t;
-        f32 shift_t[array_count(ui_pause_menu_strings)];
         s16 selection;
+        /* reserved space */
+        f32 shift_t[128];
     } ui_pause;
 
     struct entity_list entities;
@@ -201,6 +214,20 @@ local void update_and_render_ingame_game_menu_ui(struct game_state* state, struc
         }
     }
 }
+
+local void update_and_render_editor_game_menu_ui(struct game_state* state, struct software_framebuffer* framebuffer, f32 dt) {
+    if (is_key_pressed(KEY_ESCAPE)) {
+        game_state_set_ui_state(game_state, UI_STATE_PAUSE);
+        /* ready pause menu */
+        {
+            game_state->ui_pause.animation_state = 0;
+            game_state->ui_pause.transition_t    = 0;
+            game_state->ui_pause.selection       = 0;
+            zero_array(game_state->ui_pause.shift_t);
+        }
+    }
+}
+
 
 local void update_and_render_pause_game_menu_ui(struct game_state* state, struct software_framebuffer* framebuffer, f32 dt) {
     /* needs a bit of cleanup */
@@ -329,14 +356,156 @@ local void update_and_render_pause_game_menu_ui(struct game_state* state, struct
         }
     }
 }
+/* copied and pasted for now */
+local void update_and_render_pause_editor_menu_ui(struct game_state* state, struct software_framebuffer* framebuffer, f32 dt) {
+    /* needs a bit of cleanup */
+    f32 font_scale = 3;
+    /* While the real pause menu is going to be replaced with something else later obviously */
+    /* I want a nice looking menu to show off, and also the main menu is likely taking this design */
+    struct ui_pause_menu* menu_state = &state->ui_pause;
+    v2f32 item_positions[array_count(ui_pause_editor_menu_strings)] = {};
+
+    for (unsigned index = 0; index < array_count(item_positions); ++index) {
+        item_positions[index].y = 36 * (index+0.75);
+    }
+
+    f32 offscreen_x = -240;
+    f32 final_x     = 40;
+
+    u32 blur_samples = 4;
+    f32 max_blur = 1.0;
+    f32 max_grayscale = 0.8;
+
+    f32 timescale = 1.34f;
+
+    switch (menu_state->animation_state) {
+        case UI_PAUSE_MENU_TRANSITION_IN: {
+            menu_state->transition_t   += dt * timescale;
+
+            for (unsigned index = 0; index < array_count(item_positions); ++index) {
+                item_positions[index].x = lerp_f32(offscreen_x, final_x, menu_state->transition_t);
+            }
+
+            game_postprocess_blur(framebuffer, blur_samples, max_blur * menu_state->transition_t);
+            game_postprocess_grayscale(framebuffer, max_grayscale * menu_state->transition_t);
+
+            if (menu_state->transition_t >= 1.0f) {
+                menu_state->animation_state += 1;
+                menu_state->transition_t = 0;
+            }
+        } break;
+        case UI_PAUSE_MENU_NO_ANIM: {
+            for (unsigned index = 0; index < array_count(item_positions); ++index) {
+                item_positions[index].x = final_x;
+            }
+
+            game_postprocess_blur(framebuffer, blur_samples, max_blur);
+            game_postprocess_grayscale(framebuffer, max_grayscale);
+
+            for (unsigned index = 0; index < array_count(item_positions); ++index) {
+                if (index != menu_state->selection) {
+                    menu_state->shift_t[index] -= dt*4;
+                }
+            }
+            menu_state->shift_t[menu_state->selection] += dt*4;
+            for (unsigned index = 0; index < array_count(item_positions); ++index) {
+                menu_state->shift_t[index] = clamp_f32(menu_state->shift_t[index], 0, 1);
+            }
+
+            if (is_key_pressed(KEY_ESCAPE)) {
+                menu_state->animation_state = UI_PAUSE_MENU_TRANSITION_CLOSING;
+                menu_state->transition_t = 0;
+            }        
+
+            if (is_key_down_with_repeat(KEY_DOWN)) {
+                menu_state->selection++;
+                if (menu_state->selection >= array_count(item_positions)) menu_state->selection = 0;
+            }
+            if (is_key_down_with_repeat(KEY_UP)) {
+                menu_state->selection--;
+                if (menu_state->selection < 0) menu_state->selection = array_count(item_positions)-1;
+            }
+
+            if (is_key_pressed(KEY_RETURN)) {
+                switch (menu_state->selection) {
+                    case 0: {
+                        menu_state->animation_state = UI_PAUSE_MENU_TRANSITION_CLOSING;
+                        menu_state->transition_t = 0;
+                    } break;
+                    case 1: {
+                        
+                    } break;
+                    case 2: {
+                        
+                    } break;
+                    case 3: {
+                    } break;
+                    case 4: {
+                        global_game_running = false;
+                    } break;
+                }
+            }
+        } break;
+        case UI_PAUSE_MENU_TRANSITION_CLOSING: {
+            menu_state->transition_t   += dt * timescale;
+
+            for (unsigned index = 0; index < array_count(item_positions); ++index) {
+                item_positions[index].x = lerp_f32(final_x, offscreen_x, menu_state->transition_t);
+            }
+
+            game_postprocess_blur(framebuffer, blur_samples, max_blur * (1-menu_state->transition_t));
+            game_postprocess_grayscale(framebuffer, max_grayscale * (1-menu_state->transition_t));
+
+            if (menu_state->transition_t >= 1.0f) {
+                menu_state->transition_t = 0;
+                game_state_set_ui_state(game_state, state->last_ui_state);
+            }
+        } break;
+    }
+    
+    {
+        for (unsigned index = 0; index < array_count(item_positions); ++index) {
+            v2f32 draw_position = item_positions[index];
+            draw_position.x += lerp_f32(0, 20, menu_state->shift_t[index]);
+            draw_position.y += 255;
+            /* custom string drawing routine */
+            struct font_cache* font = graphics_assets_get_font_by_id(&graphics_assets, menu_fonts[MENU_FONT_COLOR_STEEL]);
+            if (index == menu_state->selection) {
+                font = graphics_assets_get_font_by_id(&graphics_assets, menu_fonts[MENU_FONT_COLOR_GOLD]);
+            }
+
+            for (unsigned character_index = 0; character_index < ui_pause_editor_menu_strings[index].length; ++character_index) {
+                f32 character_displacement_y = sinf((global_elapsed_time*2) + ((character_index+index) * 2381.2318)) * 3;
+
+                v2f32 glyph_position = draw_position;
+                glyph_position.y += character_displacement_y;
+                glyph_position.x += font->tile_width * font_scale * character_index;
+
+                software_framebuffer_draw_text(framebuffer, font, font_scale, glyph_position, string_slice(ui_pause_editor_menu_strings[index], character_index, character_index+1), color32f32(1,1,1,1), BLEND_MODE_ALPHA);
+            }
+        }
+    }
+}
 
 void update_and_render_game_menu_ui(struct game_state* state, struct software_framebuffer* framebuffer, f32 dt) {
+    if (is_key_pressed(KEY_F1)) {
+        state->in_editor ^= 1;
+    }
+
     switch (state->ui_state) {
         case UI_STATE_INGAME: {
-            update_and_render_ingame_game_menu_ui(state, framebuffer, dt);
+            if (state->in_editor) {
+                update_and_render_editor_game_menu_ui(state, framebuffer, dt);
+            } else {
+                update_and_render_ingame_game_menu_ui(state, framebuffer, dt);
+            }
         } break;
         case UI_STATE_PAUSE: {
-            update_and_render_pause_game_menu_ui(state, framebuffer, dt);
+            if (state->in_editor) {
+                update_and_render_pause_editor_menu_ui(state, framebuffer, dt);
+            } else {
+                update_and_render_pause_game_menu_ui(state, framebuffer, dt);
+            }
         } break;
             bad_case;
     }
@@ -348,19 +517,6 @@ void update_and_render_game(struct software_framebuffer* framebuffer, f32 dt) {
     }
 
     {
-#if 0
-        {
-            commands.should_clear_buffer = true;
-            commands.clear_buffer_color  = color32u8(0, 128, 0, 255);
-
-            render_commands_push_quad(&commands,  rectangle_f32(-50, 450, 100, 100), color32u8(255, 0, 0, 255), BLEND_MODE_ALPHA);
-            render_commands_push_image(&commands, graphics_assets_get_image_by_id(&graphics_assets, test_image), rectangle_f32(x, y, 96, 96), RECTANGLE_F32_NULL, color32f32(1,0,1,1), NO_FLAGS, BLEND_MODE_ALPHA);
-            render_commands_push_quad(&commands,  rectangle_f32(100, 0, 400, 400), color32u8(0, 0, 255, 128), BLEND_MODE_ALPHA);
-            render_commands_push_quad(&commands,  rectangle_f32(40, 0, 200, 200), color32u8(255, 0, 255, 128), BLEND_MODE_ALPHA);
-            render_commands_push_line(&commands,  v2f32(200, 200), v2f32(400, 400), color32u8(0, 0, 255, 255), BLEND_MODE_ALPHA);
-
-        }
-#else
         struct entity* player_entity = entity_list_dereference_entity(&game_state->entities, player_id);
         struct render_commands commands = render_commands(camera_centered(player_entity->position, 1));
         commands.should_clear_buffer = true;
@@ -372,7 +528,6 @@ void update_and_render_game(struct software_framebuffer* framebuffer, f32 dt) {
         }
 
         entity_list_render_entities(&game_state->entities, &graphics_assets, &commands, dt);
-#endif
         software_framebuffer_render_commands(framebuffer, &commands);
     }
     update_and_render_game_menu_ui(game_state, framebuffer, dt);
