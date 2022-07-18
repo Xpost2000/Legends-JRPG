38198c9806c05ac9a484bfbf78b8aed5dd5b7da1
Jerry Zhu <xpost2000cod@gmail.com>
2022-07-18 17:58:02 -0400

    ideas
    okay dialogue script later. Or start to do it later

--- save_data_def.c
+++ save_data_def.c
@@ -3,8 +3,19 @@
 
 /* delta based saving */
 /* it's meant to be basically directly serializable as a stream of bytes. */
+
+/*
+  However that's a bit painful to keep in memory.
+  
+  Right now it's planned to be a bunch of fixed size linked list nodes,
+  that go in basically a free list.
+  
+  we only need to write to it, and read from it very sparingly so this is just easier
+  all around...
+*/
+
 struct save_data {
-    /* TODO? Might change the structure? */
+    /* TODO */
 };
 
 #endif
