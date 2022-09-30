3d4ae4505c931053cfd51e6ee434a98167e95fd0
Jerry Zhu <xpost2000cod@gmail.com>
2022-09-30 00:26:07 -0400

    In theory this should work
    In practice, why would it?

--- save_data.c
+++ save_data.c
@@ -41,13 +41,24 @@ struct save_area_record_chunk {
 
 struct save_area_record_entry {
     u32 map_hash_id; /* pray I don't change hashing algorithms or everything breaks! */
-    u32 used;
 
     struct save_area_record_chunk* first;
     struct save_area_record_chunk* last;
 
     struct save_area_record_entry* next;
 };
+
+local s32 save_area_record_entry_record_count(struct save_area_record_entry* entry) {
+    s32                            count = 0;
+    struct save_area_record_chunk* chunk = entry->first;
+
+    while (chunk) {
+        count += chunk->written_entries;
+        chunk = chunk->next;
+    }
+
+    return count;
+}
 
 struct master_save_record {
     /* common things, such as all game variables */
@@ -57,12 +68,24 @@ struct master_save_record {
     */
 
     /* generic entries */
-    u32 used_entries;
     struct save_area_record_entry* first;
     struct save_area_record_entry* last;
 };
 
 struct master_save_record global_save_data = {};
+
+local s32 save_area_record_entry_count(void) {
+    s32                            count = 0; 
+    struct save_area_record_entry* entry = global_save_data.first;
+
+    while (entry) {
+        count+=1;
+        entry = entry->next;
+    }
+
+    return count;
+}
+
 
 local struct memory_arena save_arena = {};
 
@@ -97,6 +120,32 @@ void game_load_from_save_slot(s32 save_slot_id) {
     game_serialize_save(&read_serializer);
 }
 
+struct save_area_record_entry* save_record_allocate_area_entry_chunk(void) {
+    struct save_area_record_entry* new_entry = memory_arena_push(&save_arena, sizeof(*new_entry));
+
+    if (!global_save_data.last) {
+        global_save_data.first = global_save_data.last = new_entry;
+    } else {
+        global_save_data.last->next = new_entry;
+        global_save_data.last       = new_entry;
+    }
+
+    return new_entry;
+}
+
+struct save_area_record_chunk* save_area_record_chunk_allocate_entry_chunk(struct save_area_record_entry* area_entry) {
+    struct save_area_record_chunk* new_record_chunk = memory_arena_push(&save_arena, sizeof(*new_record_chunk));
+
+    if (!area_entry->last) {
+        area_entry->first = area_entry->last = new_record_chunk;
+    } else {
+        area_entry->last->next = new_record_chunk;
+        area_entry->last       = new_record_chunk;
+    }
+
+    return new_record_chunk;
+}
+
 /* binary friendly format to runtime friendly format. Oh boy... let the games begin. */
 /*
   FORMAT basically
@@ -128,6 +177,7 @@ void game_serialize_save(struct binary_serializer* serializer) {
 
     serialize_bytes(serializer, area_name_DUMMY, SAVE_SLOT_WIDGET_SAVE_NAME_LENGTH);
     serialize_bytes(serializer, area_desc_DUMMY, SAVE_SLOT_WIDGET_DESCRIPTOR_LENGTH);
+    serialize_bytes(serializer, game_state->loaded_area_name, sizeof(game_state->loaded_area_name));
 
     serialize_s32(serializer, &game_state->inventory.item_count);
     serialize_bytes(serializer, game_state->inventory.items, sizeof(*game_state->inventory.items) * game_state->inventory.item_count);
@@ -143,6 +193,78 @@ void game_serialize_save(struct binary_serializer* serializer) {
         serialize_s32(serializer, &player->magic.value);
         Serialize_Structure(serializer, player->stat_block);
         /* May need to serialize abilities later??? */
+    }
+
+    /* so this part isn't so fun since it varies a little because we may have to allocate, so we have to branch the code here... */
+    s32 area_entry_count = save_area_record_entry_count();
+    serialize_s32(serializer, &area_entry_count);
+
+    /*
+      this is version dependent! Because I use a union to store it!
+
+      While events are theoretically streams (because I don't care about random access,
+      since I want all of them at once),
+
+      it **is** possible to future proof it by simply making it a stream of data (of varying lengths),
+      as the individual record types are extremely unlikely to change (and even then, it 's way easier to
+      control version variances between individual record types as opposed to the whole size of the save record,
+
+      that might be some... Uh, shit I forgot the rest of what I wanted to type, got distracted by texting on Discord.)
+    */
+
+    if (serializer->mode == BINARY_SERIALIZER_WRITE) {
+        /* we're going to write them as if they were flat entries */ 
+        struct save_area_record_entry* current_area_entry = global_save_data.first;
+
+        for (s32 area_entry_index = 0; area_entry_index < area_entry_count; ++area_entry_index) {
+            s32 record_entries = save_area_record_entry_record_count(current_area_entry);
+
+            serialize_u32(serializer, &current_area_entry->map_hash_id);
+            serialize_s32(serializer, &record_entries);
+
+            {
+                struct save_area_record_chunk* entry_chunk = current_area_entry->first;
+
+                s32 written = 0;
+                while (entry_chunk) {
+                    serialize_bytes(serializer, entry_chunk->records, sizeof(*entry_chunk->records) * entry_chunk->written_entries);
+                    entry_chunk = entry_chunk->next;
+                }
+
+                assertion(written == record_entries && "Hmm, this doesn't smell so good to me.");
+            }
+
+            current_area_entry = current_area_entry->next;
+        }
+    } else {
+        for (s32 area_entry_index = 0; area_entry_index < area_entry_count; ++area_entry_index) {
+            struct save_area_record_entry* current_area_entry = save_record_allocate_area_entry_chunk();
+            s32                            record_entries     = save_area_record_entry_record_count(current_area_entry);
+
+            serialize_u32(serializer, &current_area_entry->map_hash_id);
+            serialize_s32(serializer, &record_entries);
+
+            {
+                s32 expected_chunks_to_write = record_entries / SAVE_RECORDS_PER_SAVE_AREA_RECORD_CHUNK + 1;
+                s32 consumed_chunks = 0;
+
+                _debugprintf("Read mode of save file, have to write %d chunks", expected_chunks_to_write);
+                for (s32 chunk_to_write = 0; chunk_to_write < expected_chunks_to_write; ++chunk_to_write) {
+                    struct save_area_record_chunk* entry_chunk = save_area_record_chunk_allocate_entry_chunk(current_area_entry);
+
+                    s32 amount_to_consume = record_entries;
+                    if (amount_to_consume >= SAVE_RECORDS_PER_SAVE_AREA_RECORD_CHUNK) {
+                        amount_to_consume = SAVE_RECORDS_PER_SAVE_AREA_RECORD_CHUNK;
+                    }
+
+                    serialize_bytes(serializer, entry_chunk->records, sizeof(*entry_chunk->records) * amount_to_consume);
+                    record_entries -= amount_to_consume;
+                    consumed_chunks += 1;
+                }
+
+                assertion(save_area_record_entry_record_count(current_area_entry) == record_entries && "Okay, that's bad...");
+            }
+        }
     }
 }
 
