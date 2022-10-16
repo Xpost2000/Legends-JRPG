57bdcca634c3cfc1629f0f23275b555b5eb43557
Jerry Zhu <xpost2000cod@gmail.com>
2022-10-16 19:08:54 -0400

    Whoops forgot to commit this

---
+++ cutscene.c
@@ -0,0 +1,64 @@
+#include "cutscene_def.c"
+
+/*
+  A real complicated situation is that cutscenes need to be able to continuously stack different level areas.
+
+  Then transition between them through fadeouts and such. Which is pretty brutal to implement...
+*/
+
+struct {
+    struct memory_arena arena;
+    struct file_buffer  file_buffer;
+    struct lisp_form    script_forms;
+    /*
+     * pre cutscene state here later, we need to copy most
+     * entities here to see if we should restore them later?
+     */
+
+    bool running;
+} cutscene_state;
+
+void cutscene_initialize(struct memory_arena* arena) {
+    cutscene_state.arena = memory_arena_push_sub_arena(arena, Megabyte(4));
+}
+
+void cutscene_open(string filepath) {
+    /* kill any movement, just in-case it was happening earlier. */
+    struct entity*   player    = game_get_player(game_state);
+    player->velocity.x               = 0; player->velocity.y = 0;
+    cutscene_state.file_buffer       = read_entire_file(memory_arena_allocator(&cutscene_state.arena), format_temp_s("./scenes/%.*s.txt", filepath.length, filepath.data));
+    cutscene_state.script_forms.list = lisp_read_string_into_forms(&scratch_arena, file_buffer_as_string(&cutscene_state.file_buffer));
+    cutscene_state.script_forms.type = LISP_FORM_LIST;
+
+    /* game_script_clear_all_awaited_scripts(); */
+    _debugprintf("why has this not been enqueued");
+    game_script_enqueue_form_to_execute(cutscene_state.script_forms);
+    _debug_print_out_lisp_code(&cutscene_state.script_forms);
+    cutscene_state.running = true;
+    _debugprintf("CUTSCENE TIME BABES!");
+}
+
+/* same as awaiting scripts but for only one script instance */
+void cutscene_update(f32 dt) {
+    /* Most of the cutscene state is actually handled by game script so this is empty. */
+
+    /*
+      However this is to update cutscene specific constructs such as the new story board or any cutscene
+      sprite objects.
+
+      Most other stuff is just handled by the game alone.
+    */
+    /* NOTE: UNUSED */
+    return;
+}
+
+bool cutscene_active(void) {
+    return cutscene_state.running;
+}
+
+void cutscene_stop(void) {
+    _debugprintf("Bye bye cutscene");
+    cutscene_state.running = false;
+    memory_arena_clear_top(&cutscene_state.arena);
+    memory_arena_clear_bottom(&cutscene_state.arena);
+}
---
+++ cutscene_def.c
@@ -0,0 +1,26 @@
+/*
+  The question is really... How extensible do I need this to be?
+
+  For the record it's just running game script sequentially with a few extensions such as a
+  parallel block (to allow for moving multiple entities at the same time. However game-script even for a lisp
+  dialect isn't particularly powerful as it does not support loops, and this is okay since all game scenes are meant to be carefully
+  constructed.)
+
+  So again there will be a slight variance on the eval system as cutscenes shall also support the "wait" command.
+
+  This might encompass the storyboard presentation system.
+
+  Also this is another "module" style file with it's own global state... Fun!
+
+  Cutscenes also need to reserve the ability to create sprite objects on demand if it should want to.
+*/
+#ifndef CUTSCENE_DEF_C
+#define CUTSCENE_DEF_C
+
+void cutscene_initialize(struct memory_arena* arena);
+void cutscene_open(string filepath);
+void cutscene_update(f32 dt);
+bool cutscene_active(void);
+void cutscene_stop(void);
+
+#endif
