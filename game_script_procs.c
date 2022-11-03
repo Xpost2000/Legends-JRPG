397cb55f5bd2faa2f8e5cb9907f53eb27e3d6e87
Jerry Zhu <xpost2000cod@gmail.com>
2022-11-02 20:56:44 -0400

    Macros to improve game_script error-pronelessness
    Added prototypes for a few new script querying functions which are
    just useful to know about.

--- game_script_procs.c
+++ game_script_procs.c
@@ -1,19 +1,76 @@
 #define GAME_LISP_FUNCTION(name) struct lisp_form name ## __script_proc (struct memory_arena* arena, struct game_state* state, struct lisp_form* arguments, s32 argument_count)
 
 
+/* NOTE: Not type checking fyi, while the game is moddable, making it fault tolerant is really annoying. So it will crash. */
+
+/* only used to pretty print required argument checks, which is the only "safe-check" that I do */
+local string numeric_strings_table_to_nineteen[] = {
+    string_literal("zero"),
+    string_literal("one"),
+    string_literal("two"),
+    string_literal("three"),
+    string_literal("four"),
+    string_literal("five"),
+    string_literal("six"),
+    string_literal("seven"),
+    string_literal("eight"),
+    string_literal("nine"),
+    string_literal("ten"),
+    string_literal("eleven"),
+    string_literal("twelve"),
+    string_literal("thirteen"),
+    string_literal("fourteen"),
+    string_literal("fifteen"),
+    string_literal("sixteen"),
+    string_literal("seventeen"),
+    string_literal("eighteen"),
+    string_literal("nineteen"),
+};
+char* _script_error_number_to_string(s32 n) {
+    if (n >= array_count(numeric_strings_table_to_nineteen)) {
+        return "[too high!]";
+    }
+
+    return numeric_strings_table_to_nineteen[n].data;
+}
+
+/*
+  Macro might need to be evolved at some point to be more fault tolerant...
+  Or we might need a "script" error log, to show up during debug mode so I can actually
+  figure out what the hell is wrong
+*/
+#define Script_Error(FUNCTION, ERRORSTRING, rest...)            \
+    do {                                                        \
+        _debugprintf(#FUNCTION " :error: " ERRORSTRING, ##rest); \
+        return LISP_nil;                                        \
+    } while(0); 
+
+#define Required_Minimum_Argument_Count(FUNCTION, COUNT)                        \
+    do {                                                                \
+        if (argument_count < COUNT) {                                   \
+            Script_Error(FUNCTION, " requires %s arguments!", _script_error_number_to_string(COUNT)); \
+        }                                                               \
+    } while(0);
+#define Required_Argument_Count(FUNCTION, COUNT)                        \
+    do {                                                                \
+        if (argument_count < COUNT) {                                   \
+            Script_Error(FUNCTION, " requires %s arguments!", _script_error_number_to_string(COUNT)); \
+        } else if (argument_count > COUNT) {                            \
+            Script_Error(FUNCTION, " has %s too many arguments!", _script_error_number_to_string(argument_count - COUNT)); \
+        }                                                               \
+    } while(0);
+#define Fatal_Script_Error(x) assertion(x)
 /*
   (follow_path GSO_HANDLE PathForm)
   
   PathForm -> '(left/down/up/right ...)
   PathForm -> '(start-x start-y) '(end-x end-y)
- */
+*/
 GAME_LISP_FUNCTION(FOLLOW_PATH) {
-    struct game_script_typed_ptr ptr = game_script_object_handle_decode(arguments[0]);
-    assertion(ptr.type == GAME_SCRIPT_TARGET_ENTITY && "Whoops! Don't know how to make non-entities follow paths!");
+    Required_Argument_Count(FOLLOW_PATH, 2)
 
-    if (argument_count < 2) {
-        return LISP_nil;
-    }
+    struct game_script_typed_ptr ptr = game_script_object_handle_decode(arguments[0]);
+    Fatal_Script_Error(ptr.type == GAME_SCRIPT_TARGET_ENTITY && "Whoops! Don't know how to make non-entities follow paths!");
 
     v2f32* path       = 0;
     s32    path_count = 0;
@@ -26,7 +83,7 @@ GAME_LISP_FUNCTION(FOLLOW_PATH) {
         path = memory_arena_push(&scratch_arena, 0);
 
         struct lisp_form* defined_path = &arguments[1];
-        assertion(defined_path->type == LISP_FORM_LIST && "this is not a path I guess.");
+        Fatal_Script_Error(defined_path->type == LISP_FORM_LIST && "this is not a path I guess.");
 
         f32 x_cursor = target_entity->position.x/TILE_UNIT_SIZE;
         f32 y_cursor = target_entity->position.y/TILE_UNIT_SIZE;
@@ -93,11 +150,8 @@ GAME_LISP_FUNCTION(FOLLOW_PATH) {
 }
 
 GAME_LISP_FUNCTION(ENTITY_LOOK_AT) {
-    /* The only use case right now is looking at other entities */
-    if (argument_count < 2) {
-        _debugprintf("entity_look_at requires two arguments");
-        return LISP_nil;
-    }
+    Required_Argument_Count(ENTITY_LOOK_AT, 2);
+
     struct game_script_typed_ptr ptr = game_script_object_handle_decode(arguments[0]);
     assertion(ptr.type == GAME_SCRIPT_TARGET_ENTITY && "This only works on entities!");
     struct entity* target_entity = game_dereference_entity(state, ptr.entity_id);
@@ -122,16 +176,28 @@ GAME_LISP_FUNCTION(ENTITY_LOOK_AT) {
 
     return LISP_nil;
 }
+GAME_LISP_FUNCTION(ENTITY_IS_DEAD) {
+    Required_Argument_Count(ENTITY_IS_DEAD, 1);
+    return LISP_nil;
+}
+
+GAME_LISP_FUNCTION(ENTITY_GET_HEALTH) {
+    Required_Argument_Count(ENTITY_IS_DEAD, 1);
+    return LISP_nil;
+}
+
+GAME_LISP_FUNCTION(ENTITY_GET_HEALTH_PERCENT) {
+    Required_Argument_Count(ENTITY_IS_DEAD, 1);
+    return LISP_nil;
+}
 
 GAME_LISP_FUNCTION(ENTITY_SPEAK) {
-    if (argument_count < 2) {
-        _debugprintf("entity_speak requires two arguments");
-        return LISP_nil;
-    }
+    Required_Argument_Count(ENTITY_SPEAK, 2);
+
     struct game_script_typed_ptr ptr = game_script_object_handle_decode(arguments[0]);
-    assertion(ptr.type == GAME_SCRIPT_TARGET_ENTITY && "This only works on entities!");
+    Fatal_Script_Error(ptr.type == GAME_SCRIPT_TARGET_ENTITY && "This only works on entities!");
     struct entity* target_entity = game_dereference_entity(state, ptr.entity_id);
-    assertion(target_entity && "no entity?");
+    Fatal_Script_Error(target_entity && "no entity?");
 
     string text;
     if (lisp_form_get_string(arguments[1], &text)) {
@@ -143,6 +209,7 @@ GAME_LISP_FUNCTION(ENTITY_SPEAK) {
 }
 
 GAME_LISP_FUNCTION(KILL_ALL) {
+    Required_Argument_Count(KILL_ALL, 0);
     /* or any player entity rather, so we'll check for friendly */
     /*
       NOTE:
@@ -174,56 +241,53 @@ GAME_LISP_FUNCTION(KILL_ALL) {
 
 GAME_LISP_FUNCTION(OBJ_ACTIVATIONS) {
     /* expect an object handle. Not checking right now */
+    Required_Argument_Count(OBJ_ACTIVATIONS, 1);
+    struct game_script_typed_ptr object_ptr = game_script_object_handle_decode(arguments[0]);
 
-    _debugprintf("Hi I was called with : %d args", argument_count);
-    if (argument_count == 1) {
-        struct game_script_typed_ptr object_ptr = game_script_object_handle_decode(arguments[0]);
-
-        switch (object_ptr.type) {
-            case GAME_SCRIPT_TARGET_TRIGGER: {
-                struct trigger* trigger = (struct trigger*)object_ptr.ptr;
-                _debugprintf("requested activation # for trigger: (%d)", trigger->activations);
-                return lisp_form_integer(trigger->activations);
-            } break;
-                bad_case;
-        }
+    switch (object_ptr.type) {
+        case GAME_SCRIPT_TARGET_TRIGGER: {
+            struct trigger* trigger = (struct trigger*)object_ptr.ptr;
+            _debugprintf("requested activation # for trigger: (%d)", trigger->activations);
+            return lisp_form_integer(trigger->activations);
+        } break;
+            bad_case;
     }
 
     return LISP_nil;
 }
 
 GAME_LISP_FUNCTION(OPEN_SHOP) {
-    if (argument_count == 1) {
-        string shop_name = {};
-        assert(lisp_form_get_string(arguments[0], &shop_name));
-        game_begin_shopping(shop_name);
-    }
+    Required_Argument_Count(OPEN_SHOP, 1);
+
+    string shop_name = {};
+    assert(lisp_form_get_string(arguments[0], &shop_name));
+    game_begin_shopping(shop_name);
 
     return LISP_nil;
 }
 
 GAME_LISP_FUNCTION(random) {
     /* expect an object handle. Not checking right now */
-    if (argument_count == 1) {
-        if (arguments[0].type == LISP_FORM_NUMBER) {
-            if (arguments[0].is_real) {
-                f32 arg = 1.0f;
+    Required_Argument_Count(RANDOM, 1);
+    
+    if (arguments[0].type == LISP_FORM_NUMBER) {
+        if (arguments[0].is_real) {
+            f32 arg = 1.0f;
 
-                lisp_form_get_f32(arguments[0], &arg);
-                f32 return_value = random_float(&state->rng) * arg;
+            lisp_form_get_f32(arguments[0], &arg);
+            f32 return_value = random_float(&state->rng) * arg;
 
-                return lisp_form_real(return_value);
-            } else {
-                s32 arg = 0;
-
-                lisp_form_get_s32(arguments[0], &arg);
-                s32 return_value = random_ranged_integer(&state->rng, 0, arg);
-
-                return lisp_form_integer(return_value);
-            }
+            return lisp_form_real(return_value);
         } else {
-            return LISP_nil;
+            s32 arg = 0;
+
+            lisp_form_get_s32(arguments[0], &arg);
+            s32 return_value = random_ranged_integer(&state->rng, 0, arg);
+
+            return lisp_form_integer(return_value);
         }
+    } else {
+        return LISP_nil;
     }
 
     return LISP_nil;
@@ -233,6 +297,8 @@ GAME_LISP_FUNCTION(DLG_NEXT) {
     return LISP_nil;
 }
 GAME_LISP_FUNCTION(GAME_IS_WEATHER) {
+    Required_Argument_Count(GAME_IS_WEATHER, 1);
+
     struct lisp_form weather_type = arguments[0];
     if (lisp_form_symbol_matching(weather_type, string_literal("snow"))) {
         return lisp_form_produce_truthy_value_form(state->weather.features & WEATHER_SNOW);
@@ -247,26 +313,31 @@ GAME_LISP_FUNCTION(GAME_IS_WEATHER) {
     return LISP_nil;
 }
 GAME_LISP_FUNCTION(GAME_START_RAIN) {
+    Required_Argument_Count(GAME_START_RAIN, 0);
     _debugprintf("game_start_rain script");
     weather_start_rain(state);
     return LISP_nil;
 }
 GAME_LISP_FUNCTION(GAME_STOP_RAIN) {
+    Required_Argument_Count(GAME_STOP_RAIN, 0);
     _debugprintf("game_stop_rain script");
     weather_stop_rain(state);
     return LISP_nil;
 }
 GAME_LISP_FUNCTION(GAME_START_SNOW) {
+    Required_Argument_Count(GAME_START_SNOW, 0);
     _debugprintf("game_start_snow script");
     weather_start_snow(state);
     return LISP_nil;
 }
 GAME_LISP_FUNCTION(GAME_STOP_SNOW) {
+    Required_Argument_Count(GAME_STOP_SNOW, 0);
     _debugprintf("game_stop_snow script");
     weather_stop_snow(state);
     return LISP_nil;
 }
 GAME_LISP_FUNCTION(GAME_SET_REGION_NAME) {
+    Required_Argument_Count(GAME_SET_REGION_NAME, 2);
     _debugprintf("set region name");
     struct lisp_form* str_name     = &arguments[0];
     struct lisp_form* str_sub_name = &arguments[1];
@@ -287,69 +358,72 @@ GAME_LISP_FUNCTION(GAME_SET_REGION_NAME) {
 }
 
 GAME_LISP_FUNCTION(GAME_SET_ENVIRONMENT_COLORS) {
-    if (argument_count == 1) {
-        struct lisp_form* argument = &arguments[0];
+    Required_Argument_Count(GAME_SET_ENVIRONMENT_COLORS, 1);
 
-        if (argument->type == LISP_FORM_SYMBOL) {
-            if (lisp_form_symbol_matching(*argument, string_literal("night"))) {
-                global_color_grading_filter = COLOR_GRADING_NIGHT;
-            } else if (lisp_form_symbol_matching(*argument, string_literal("dawn"))) {
-                global_color_grading_filter = COLOR_GRADING_DAWN;
-            } else if (lisp_form_symbol_matching(*argument, string_literal("midnight"))) {
-                global_color_grading_filter = COLOR_GRADING_DARKEST;
-            } else if (lisp_form_symbol_matching(*argument, string_literal("day"))) {
-                global_color_grading_filter = COLOR_GRADING_DAY;
-            }
-        } else {
-            struct lisp_form* r = lisp_list_nth(argument, 0);
-            struct lisp_form* g = lisp_list_nth(argument, 1);
-            struct lisp_form* b = lisp_list_nth(argument, 2);
-            struct lisp_form* a = lisp_list_nth(argument, 3);
+    struct lisp_form* argument = &arguments[0];
 
-            f32 color_r;
-            f32 color_g;
-            f32 color_b;
-            f32 color_a;
-
-            if (!(r && lisp_form_get_f32(*r, &color_r))) color_r = 255.0f;
-            if (!(g && lisp_form_get_f32(*g, &color_g))) color_g = 255.0f;
-            if (!(b && lisp_form_get_f32(*b, &color_b))) color_b = 255.0f;
-            if (!(a && lisp_form_get_f32(*a, &color_a))) color_a = 255.0f;
-
-            global_color_grading_filter.r = color_r;
-            global_color_grading_filter.g = color_g;
-            global_color_grading_filter.b = color_b;
-            global_color_grading_filter.a = color_a;
+    if (argument->type == LISP_FORM_SYMBOL) {
+        if (lisp_form_symbol_matching(*argument, string_literal("night"))) {
+            global_color_grading_filter = COLOR_GRADING_NIGHT;
+        } else if (lisp_form_symbol_matching(*argument, string_literal("dawn"))) {
+            global_color_grading_filter = COLOR_GRADING_DAWN;
+        } else if (lisp_form_symbol_matching(*argument, string_literal("midnight"))) {
+            global_color_grading_filter = COLOR_GRADING_DARKEST;
+        } else if (lisp_form_symbol_matching(*argument, string_literal("day"))) {
+            global_color_grading_filter = COLOR_GRADING_DAY;
         }
+    } else {
+        struct lisp_form* r = lisp_list_nth(argument, 0);
+        struct lisp_form* g = lisp_list_nth(argument, 1);
+        struct lisp_form* b = lisp_list_nth(argument, 2);
+        struct lisp_form* a = lisp_list_nth(argument, 3);
+
+        f32 color_r;
+        f32 color_g;
+        f32 color_b;
+        f32 color_a;
+
+        if (!(r && lisp_form_get_f32(*r, &color_r))) color_r = 255.0f;
+        if (!(g && lisp_form_get_f32(*g, &color_g))) color_g = 255.0f;
+        if (!(b && lisp_form_get_f32(*b, &color_b))) color_b = 255.0f;
+        if (!(a && lisp_form_get_f32(*a, &color_a))) color_a = 255.0f;
+
+        global_color_grading_filter.r = color_r;
+        global_color_grading_filter.g = color_g;
+        global_color_grading_filter.b = color_b;
+        global_color_grading_filter.a = color_a;
     }
 
     return LISP_nil;
 }
 GAME_LISP_FUNCTION(GAME_INPUT_ENABLED) {
+    Required_Argument_Count(GAME_INPUT_ENABLED, 0);
     return lisp_form_produce_truthy_value_form(!disable_game_input);
 }
 GAME_LISP_FUNCTION(GAME_DISABLE_INPUT) {
+    Required_Argument_Count(GAME_DISABLE_INPUT, 0);
     disable_game_input = true;
     return lisp_form_produce_truthy_value_form(!disable_game_input);
 }
 GAME_LISP_FUNCTION(GAME_ENABLE_INPUT) {
+    Required_Argument_Count(GAME_ENABLE_INPUT, 0);
     disable_game_input = false;
     return lisp_form_produce_truthy_value_form(!disable_game_input);
 }
 GAME_LISP_FUNCTION(GAME_MESSAGE_QUEUE) {
-    if (argument_count == 1) {
-        struct lisp_form* argument = &arguments[0];
+    Required_Argument_Count(GAME_MESSAGE_QUEUE, 1);
 
-        if (argument->type == LISP_FORM_STRING) {
-            game_message_queue(argument->string);
-        }
+    struct lisp_form* argument = &arguments[0];
+
+    if (argument->type == LISP_FORM_STRING) {
+        game_message_queue(argument->string);
     }
 
     return LISP_nil;
 }
 
 GAME_LISP_FUNCTION(GAME_OPEN_CONVERSATION) {
-    assertion(argument_count >= 1 && "need at least one argument");
+    Required_Minimum_Argument_Count(GAME_OPEN_CONVERSATION, 1);
 
     string conversation_path_partial = {};
     lisp_form_get_string(arguments[0], &conversation_path_partial);
@@ -368,6 +442,8 @@ return LISP_nil;
 }
 
 GAME_LISP_FUNCTION(GAME_START_FIGHT_WITH) {
+    Required_Minimum_Argument_Count(GAME_START_FIGHT_WITH, 1);
+
     for (unsigned argument_index = 0; argument_index < argument_count; ++argument_index) {
         struct game_script_typed_ptr ptr       = game_script_object_handle_decode(arguments[argument_index]);
         struct entity*               to_fight  = game_dereference_entity(state, ptr.entity_id);
@@ -377,7 +453,7 @@ GAME_LISP_FUNCTION(GAME_START_FIGHT_WITH) {
 }
 
 GAME_LISP_FUNCTION(START_CUTSCENE) {
-    assertion(argument_count >= 1 && "need at least one argument");
+    Required_Argument_Count(START_CUTSCENE, 1);
 
     string cutscene_path = {};
     lisp_form_get_string(arguments[0], &cutscene_path);
@@ -386,11 +462,14 @@ GAME_LISP_FUNCTION(START_CUTSCENE) {
     return LISP_nil;
 }
 GAME_LISP_FUNCTION(END_CUTSCENE) {
+    Required_Argument_Count(END_CUTSCENE, 0);
     cutscene_stop();
     return LISP_nil;
 }
 
 GAME_LISP_FUNCTION(CUTSCENE_LOAD_AREA) {
+    Required_Argument_Count(CUTSCENE_LOAD_AREA, 1);
+
     string cutscene_area_path = {};
     lisp_form_get_string(arguments[0], &cutscene_area_path);
     cutscene_load_area(cutscene_area_path);
@@ -403,11 +482,12 @@ GAME_LISP_FUNCTION(CUTSCENE_UNLOAD_AREA) {
     return LISP_nil;
 }
 GAME_LISP_FUNCTION(CAMERA_PUTAT_FORCE) {
-    _debugprintf("???? CAMERA PUTAT FORCE");
-    _debugprintf("%f, %f\n", game_state->camera.xy.x, game_state->camera.xy.y);
+    Required_Argument_Count(CAMERA_PUTAT_FORCE, 1);
+
     assertion(lisp_form_get_f32(arguments[0].list.forms[0], &game_state->camera.xy.x) && "camera x fail?");
     assertion(lisp_form_get_f32(arguments[0].list.forms[1], &game_state->camera.xy.y) && "camera y fail?");
     _debugprintf("%f, %f\n", game_state->camera.xy.x, game_state->camera.xy.y);
+
     return LISP_nil;
 }
 
@@ -422,6 +502,11 @@ static struct game_script_function_builtin script_function_table[] = {
     GAME_LISP_FUNCTION(OBJ_ACTIVATIONS),
     GAME_LISP_FUNCTION(GAME_START_RAIN),
     GAME_LISP_FUNCTION(ENTITY_LOOK_AT),
+
+    GAME_LISP_FUNCTION(ENTITY_IS_DEAD),
+    GAME_LISP_FUNCTION(ENTITY_GET_HEALTH),
+    GAME_LISP_FUNCTION(ENTITY_GET_HEALTH_PERCENT),
+
     GAME_LISP_FUNCTION(GAME_STOP_RAIN),
     GAME_LISP_FUNCTION(GAME_START_SNOW),
     GAME_LISP_FUNCTION(FOLLOW_PATH),
@@ -450,3 +535,7 @@ static struct game_script_function_builtin script_function_table[] = {
       I might contextually change functions based on cutscene-ness but who knows?
     */
 };
+
+#undef Fatal_Script_Error
+#undef Script_Error
+#undef Required_Argument_Count
