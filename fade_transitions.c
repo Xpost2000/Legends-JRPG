e37c22762ec0fed3fb1e5781495da9206aa22e66
Jerry Zhu <xpost2000cod@gmail.com>
2022-11-04 18:57:31 -0400

    Okay, let's add a few more transition types now that I know it works.
    Then the next important order of business? Text markup or something
    like that!
    
    (Man, I'm really procrastinating that particle system...)

--- fade_transitions.c
+++ fade_transitions.c
@@ -6,17 +6,31 @@
 */
 
 enum transition_fader_type {
+    /*
+      NOTE:
+      While crossfade scene is also a type of transition fader technically, it
+      doesn't really obey the rules of any other type of transition (as there's no "fade out" by definition),
+      so it's staying as a special effect
+    */
     TRANSITION_FADER_TYPE_NONE,
     TRANSITION_FADER_TYPE_COLOR,
+
+    TRANSITION_FADER_TYPE_VERTICAL_SLIDE,
+    TRANSITION_FADER_TYPE_HORIZONTAL_SLIDE,
+
+    TRANSITION_FADER_TYPE_SHUTEYE_SLIDE,
+    TRANSITION_FADER_TYPE_CURTAINCLOSE_SLIDE,
+
     TRANSITION_FADER_TYPE_COUNT,
 };
 
 typedef void (*transition_on_delay_finish_callback)(void* data);
 typedef void (*transition_on_finish_callback)(void* data);
+typedef void (*transition_on_start_callback)(void* data);
 
 void ___transition_stubs(void* data){}
 
-#define TRANSITION_MAX_CALLBACK_DATA (Bytes(512))
+#define TRANSITION_MAX_CALLBACK_DATA (Bytes(128))
 struct transition_fader_state {
     u32                                 type;
     union color32f32                    color;
@@ -26,6 +40,8 @@ struct transition_fader_state {
     bool                                forwards;
     transition_on_delay_finish_callback on_delay_finish;
     transition_on_finish_callback       on_finish;
+    transition_on_start_callback        on_start;
+    u8                                  callback_data_on_start[TRANSITION_MAX_CALLBACK_DATA];
     u8                                  callback_data_on_finish[TRANSITION_MAX_CALLBACK_DATA];
     u8                                  callback_data_on_delay_finish[TRANSITION_MAX_CALLBACK_DATA];
 };
@@ -77,6 +93,14 @@ local void transition_register_on_finish(transition_on_finish_callback callback,
     }
     memory_copy(data, state->callback_data_on_finish, data_length);
 }
+local void transition_register_on_start(transition_on_start_callback callback, u8* data, size_t data_length) {
+    struct transition_fader_state* state = &global_transition_fader_state;
+    state->on_start                     = callback;
+    if (data_length > TRANSITION_MAX_CALLBACK_DATA) {
+        data_length = TRANSITION_MAX_CALLBACK_DATA;   
+    }
+    memory_copy(data, state->callback_data_on_start, data_length);
+}
 
 local void do_color_transition_in(union color32f32 target_color, f32 delay_time, f32 time) {
     struct transition_fader_state* state = &global_transition_fader_state;
@@ -86,8 +110,11 @@ local void do_color_transition_in(union color32f32 target_color, f32 delay_time,
     state->time                          = 0;
     state->delay_time                    = delay_time;
     state->forwards                      = true;
+
     transition_register_on_delay_finish(___transition_stubs, NULL, 0);
     transition_register_on_finish(___transition_stubs, NULL, 0);
+    transition_register_on_start(___transition_stubs, NULL, 0);
+
     _debugprintf("Starting a color fade in!");
 }
 
@@ -99,18 +126,28 @@ local void do_color_transition_out(union color32f32 target_color, f32 delay_time
     state->time                          = 0;
     state->delay_time                    = delay_time;
     state->forwards                      = false;
+
     transition_register_on_delay_finish(___transition_stubs, NULL, 0);
     transition_register_on_finish(___transition_stubs, NULL, 0);
+    transition_register_on_start(___transition_stubs, NULL, 0);
+
     _debugprintf("Starting a color fade out!");
 }
 
 local void update_and_render_color_fades(struct game_state* state, struct transition_fader_state* fader_state, struct software_framebuffer* framebuffer, f32 effective_t);
+local void update_and_render_slide_fades(struct game_state* state, struct transition_fader_state* fader_state, struct software_framebuffer* framebuffer, f32 effective_t);
 local void transitions_update_and_render(struct game_state* state, struct software_framebuffer* framebuffer, f32 dt) {
     struct transition_fader_state* transition_state = &global_transition_fader_state;
 
     if (transition_state->type == TRANSITION_FADER_TYPE_NONE) {
         return;
     }
+
+    if (transition_state->time == 0.0f) {
+        transition_state->on_start(transition_state->callback_data_on_start);
+    }
+
+    f32 effective_t = clamp_f32((transition_state->time / transition_state->max_time), 0, 1);
 
     if (transition_state->delay_time > 0) {
         transition_state->delay_time -= dt;
@@ -119,15 +156,6 @@ local void transitions_update_and_render(struct game_state* state, struct softwa
             transition_state->on_delay_finish(transition_state->callback_data_on_delay_finish);
         }
     } else {
-        f32 effective_t = clamp_f32((transition_state->time / transition_state->max_time), 0, 1);
-
-        switch (transition_state->type) {
-            case TRANSITION_FADER_TYPE_COLOR: {
-                update_and_render_color_fades(state, transition_state, framebuffer, effective_t);
-            } break;
-                bad_case;
-        }
-
         if (transition_state->time < transition_state->max_time) {
             transition_state->time += dt;
 
@@ -139,6 +167,14 @@ local void transitions_update_and_render(struct game_state* state, struct softwa
             /* ? */
         }
     }
+
+    switch (transition_state->type) {
+        case TRANSITION_FADER_TYPE_COLOR: {
+            update_and_render_color_fades(state, transition_state, framebuffer, effective_t);
+        } break;
+            bad_case;
+    }
+
 }
 
 local void update_and_render_color_fades(struct game_state* state, struct transition_fader_state* fader_state, struct software_framebuffer* framebuffer, f32 effective_t) {
@@ -148,6 +184,17 @@ local void update_and_render_color_fades(struct game_state* state, struct transi
 
     union color32f32 render_color = fader_state->color;
     render_color.a                = lerp_f32(fader_state->color.a, 0, effective_t);
+
+    software_framebuffer_draw_quad(framebuffer, rectangle_f32(0, 0, framebuffer->width, framebuffer->height), color32f32_to_color32u8(render_color), BLEND_MODE_ALPHA);
+}
+
+local void update_and_render_slide_fades(struct game_state* state, struct transition_fader_state* fader_state, struct software_framebuffer* framebuffer, f32 effective_t) {
+    if (fader_state->forwards) {
+        effective_t = 1 - effective_t;
+    }
+
+    union color32f32 render_color = fader_state->color;
+    f32 x_position;
 
     software_framebuffer_draw_quad(framebuffer, rectangle_f32(0, 0, framebuffer->width, framebuffer->height), color32f32_to_color32u8(render_color), BLEND_MODE_ALPHA);
 }
